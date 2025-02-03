6c55399422e9e1977244f219ae8af4fe2418b4bc
Calle <carl.jonsen@yh.nackademin.se>
2025-02-03 11:45:55 +0100

    pictures

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -5,7 +5,7 @@ export const Home: React.FC = () => (
   <section className="home-section container">
     <div className="row align-items-center">
       <div className="col-md-6 text-center">
-        <img src="/public/assets/DSC01149_Original.jpg" alt="Carl Jonsén" className="img-fluid rounded-circle" />
+        <img src="./assets/DSC01149_Original.jpg" alt="Carl Jonsén" className="img-fluid rounded-circle" />
       </div>
       <div className="col-md-6">
         <h1 className="display-5">Welcome to My First Website</h1>
